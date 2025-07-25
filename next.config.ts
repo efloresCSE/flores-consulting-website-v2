0b75bff08250d7a8b948d406fad346744e2c60f1
efloresCSE <eflorescse@gmail.com>
2025-07-24 21:03:14 -0700

    fixing links

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+
 const nextConfig = {
   output: 'export',
-
+  basepath: isProduction ? 'flores-consulting-website-v2' : '',
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
   images: {
